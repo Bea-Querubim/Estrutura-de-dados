13e697fbe93613a9d2aa82e30e66a8df160f65ab
Bea-Querubim <beatrizq.batista@outlook.com.br>
2024-06-05 23:40:30 -0300

    commit init pilha

---
+++ Java/src/main/java/aulas/Pilha.java
@@ -0,0 +1,16 @@
+/*  desenho de entendimento de uma pilha -> é como um vetor porem na vertical
+ *
+ * [5]   []  -->topo da pilha
+ * [4]   []
+ * [3]   []
+ * [2]   [valor3]
+ * [1]   [valor2]
+ * [0]   [valor1] --> base da pilha
+ * 
+ * Quando precisamos empilhar (coloca em cima da pilha, ou seja no topo da pilha) --> push
+ * 
+ * Quando precisamos desempilhar --> pop. (pega o 1 valor do topo da pilha (ou o ultimo valor da pilha, mesma coisa) )
+ * 
+ * Comportamento dapilha --> LIFO
+ * Last In, First Out  -> ultimo a entrar (empilhar), primeiro a sair (desempilhar)
+*/
